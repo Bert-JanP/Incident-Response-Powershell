bf58aa019aa52ea7e1a67b5f21b5291fa7ea980c
Bert-Janp <bertjanpals@gmail.com>
2023-10-24 21:50:52 +0200

    Update

---
+++ Scripts/CollectPnPDevices.ps1
@@ -0,0 +1,26 @@
+$deviceClasses = @(
+    'Net',
+    'USB',
+    'Volume',
+    'Printer',
+    'HIDClass'
+)
+
+$currentDate = Get-Date -Format "yyyy-MM-dd"
+
+foreach ($class in $deviceClasses) {
+    $outputFileName = "$class Devices - $currentDate.txt"
+    $outputPath = Join-Path -Path . -ChildPath $outputFileName
+
+    Write-Host "Collecting devices in $class class. Saving results to $outputFileName..."
+    
+    $deviceInfo = Get-PnpDevice -Class $class | ForEach-Object {
+        "Device ID: $($_.InstanceId)"
+        "Device Description: $($_.Description)"
+        "Device Status: $($_.Status)"
+        "-------------------"
+    }
+
+    $deviceInfo | Out-File -FilePath $outputPath
+    Write-Host "Results saved to $outputPath"
+}
