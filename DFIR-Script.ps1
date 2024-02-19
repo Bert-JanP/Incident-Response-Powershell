e99e94ef80c3e5257952197a9c9afb88fcd80085
edmond-nader <49365047+edmond-nader@users.noreply.github.com>
2024-02-19 17:45:24 +0200

    Update DFIR-Script.ps1 - Get-RunningServices
    Changing as well the FolderName for the txt output for runningservices

--- DFIR-Script.ps1
+++ DFIR-Script.ps1
@@ -310,7 +310,7 @@ function Get-RecentlyInstalledSoftwareEventLogs {
 
 function Get-RunningServices {
     Write-Host "Collecting Running Services..."
-    $ApplicationFolder = "$FolderCreation\Applications"
+    $ApplicationFolder = "$FolderCreation\Services"
     $ProcessOutput = "$ApplicationFolder\RunningServices.txt"
     Get-Service | Where-Object {$_.Status -eq "Running"} | format-list | Out-File -Force -FilePath $ProcessOutput
 	$CSVExportLocation = "$CSVOutputFolder\RunningServices.csv"
