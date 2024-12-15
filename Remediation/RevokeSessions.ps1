6fc94db9a92a66c78de43c1ff456ebb4f973e595
Bert-JanP <bertjanpals@gmail.com>
2024-12-15 17:17:41 +0100

    Initial commit remediation

---
+++ Remediation/RevokeSessions.ps1
@@ -0,0 +1,21 @@
+# Docs: https://learn.microsoft.com/en-us/graph/api/user-revokesigninsessions?view=graph-rest-1.0&tabs=http
+
+Connect-MgGraph -Scopes User.RevokeSessions.All	
+
+Import-Module Microsoft.Graph.Users.Actions
+
+# List of UPNs that need to be revoked.
+
+$users = ('user1@kqlquery.com', 'user2@kqlquery.com')
+
+Write-Output "Start revoking sessions..."
+
+# Revoke user sessions
+foreach ($user in $users) {
+    $result = Revoke-MgUserSignInSession -UserId $user
+    if ($result) {
+        Write-Host "Successfully revoked sessions for $user" -ForegroundColor Green
+    } else {
+        Write-Host "Failed to revoke sessions for $user" -ForegroundColor Red
+    }
+}
