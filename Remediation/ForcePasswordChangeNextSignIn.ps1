a8fb812304d70436f5c4a3728e8da4f0ebecb571
Bert-Jan <91258706+Bert-JanP@users.noreply.github.com>
2024-12-22 14:54:47 +0100

    Create ForcePasswordChangeNextSignIn.ps1

---
+++ Remediation/ForcePasswordChangeNextSignIn.ps1
@@ -0,0 +1,21 @@
+# Docs: https://learn.microsoft.com/en-us/graph/api/authenticationmethod-resetpassword?view=graph-rest-1.0&tabs=http
+# Changes the users password with a new random password, this new password has to be changed on the first SignIn of the user.
+
+Connect-MgGraph -Scopes UserAuthenticationMethod.ReadWrite.All
+
+Import-Module Microsoft.Graph.Users.Actions
+
+# List of UPNs that need a password change
+
+$users = ('user1@kqlquery.com', 'user2@kqlquery.com')
+
+Write-Output "Start Force Password Change..."
+
+# Force Password Change
+foreach ($user in $users) {
+    $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    $randomString = -join ((1..30) | ForEach-Object { $chars[(Get-Random -Maximum $chars.Length)] })
+    $method = Get-MgUserAuthenticationPasswordMethod -UserId $user
+    Reset-MgUserAuthenticationMethodPassword -UserId $user -AuthenticationMethodId $method.id -NewPassword $randomString
+    Write-Host "Set force Change Password Next SignIn for $user. Temporary password is set to $randomString" -ForegroundColor Green
+}
