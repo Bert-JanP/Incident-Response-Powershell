e763bedad092ac07cd0f1b6f4dad7196053ba2c9
Bert-Jan <91258706+Bert-JanP@users.noreply.github.com>
2025-01-01 19:08:22 +0100

    Acquisition Update

---
+++ Acquisition/CollectWindowsEvents.ps1
@@ -0,0 +1,24 @@
+$ExecutionDate = $(get-date -f yyyy-MM-dd)
+$OutputName = "WindowsEvents-$ExecutionDate.csv"
+# Initialize an empty array to store event log entries
+$logEntries = @()
+
+$eventLogs = 'Application', 'System', 'Security'
+
+# Iterate through each event log
+foreach ($logName in $eventLogs) {
+    # Get event log entries for the specified log name
+    $entries = Get-EventLog -LogName $logName
+
+    # Append entries to the logEntries array
+    $logEntries += $entries
+}
+
+# Export the event log entries to a CSV file
+$logEntries | Export-Csv -Path $OutputName -NoTypeInformation
+if (Test-Path -Path $OutputName) {
+    $folderPath = (Get-Item $OutputName).DirectoryName
+    Write-Host "Output File Location: $folderPath\$OutputName"
+} else {
+    Write-Host "File does not exist."
+}
